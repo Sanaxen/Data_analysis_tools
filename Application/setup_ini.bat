c846420c0b7976fdeb1fdc77db005e65ea2d2a7e
Sanaxen <bccatfalcon@gmail.com>
2020-08-22 13:36:55 +0900

    Update setup_ini.bat

--- Application/setup_ini.bat
+++ Application/setup_ini.bat
@@ -1,7 +1,10 @@
-set R_INSTALL_PATH=C:\Program Files\R\R-3.6.1
+set R_INSTALL_PATH=D:\Program Files\R\R-3.6.1
 set GNUPLOT_PATH=C:\Program Files\gnuplot\bin
 set GRAPHVIZ_PATH=C:\Program Files (x86)\Graphviz2.38\bin
 set RTOOL_PATH=C:\Rtools
+
+:#https://github.com/Sanaxen/deepAR
+set DEEPAR_APP=C:\Users\bccat\Desktop\deepARapp\deepAR_application
 
  
 set PYTORCH_CUDA=%~dp0\DDS2\bin\gpu_version
