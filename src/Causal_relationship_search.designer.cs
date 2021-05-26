64dc25f0317496d77be9d81285ac4e7b5eb60603
Sanaxen <bccatfalcon@gmail.com>
2021-05-26 23:29:28 +0900

    Update Causal_relationship_search.designer.cs

--- src/Causal_relationship_search.designer.cs
+++ src/Causal_relationship_search.designer.cs
@@ -1254,7 +1254,7 @@
             this.button19.Name = "button19";
             this.button19.Size = new System.Drawing.Size(75, 23);
             this.button19.TabIndex = 74;
-            this.button19.Text = "button19";
+            this.button19.Text = "default";
             this.button19.UseVisualStyleBackColor = true;
             this.button19.Click += new System.EventHandler(this.button19_Click);
             // 
