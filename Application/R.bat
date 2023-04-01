16bfca75e01db2dd9e84bdc7124c55454a893cc2
Sanaxen <bccatfalcon@gmail.com>
2023-04-01 15:27:45 +0900

    Update R.bat

--- Application/R.bat
+++ Application/R.bat
@@ -9,9 +9,6 @@ set MECAB_PATH=%~dp0\DDS2\bin\MeCab
 set PATH=%MECAB_PATH%\bin;%PATH%
 set MECABRC=%MECAB_PATH%\etc\mecabrc
 
-set RTOOL_PATH=%~dp0\DDS2\bin\rtools40
-set RTOOLS40_HOME=%~dp0\DDS2\bin\rtools40
-
 set APPDATA_DDS=%~dp0\DDS2\bin\AppData\Roaming
 
 %R_INSTALL_PATH%\bin\x64\Rgui.exe
