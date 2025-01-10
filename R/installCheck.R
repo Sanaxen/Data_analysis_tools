b34a4290643d8e73523b50b8be49feaadd5460e3
Sanaxen <bccatfalcon@gmail.com>
2025-01-10 11:37:50 +0900

    Update installCheck.R

--- R/installCheck.R
+++ R/installCheck.R
@@ -21,47 +21,47 @@ targetPackages <- c('forecast', 'ellipse', 'magrittr','randomForest','car','dply
 , 'dHSIC')
 
 newPackages <- targetPackages[!(targetPackages %in% installed.packages()[,"Package"])]
-if(length(newPackages)) install.packages(newPackages, repos = "http://cran.us.r-project.org", lib=install_libpath)
+if(length(newPackages)) install.packages(newPackages, repos = "http://cran.us.r-project.org", lib=install_libpath, type = "binary")
 for(package in targetPackages) library(package, character.only = T)
 
 
 webshot::install_phantomjs()
-install.packages("RMeCab", repos = "http://rmecab.jp/R", lib=install_libpath) 
+install.packages("RMeCab", repos = "http://rmecab.jp/R", lib=install_libpath, type = "binary") 
 
-#install.packages("wordcloud2", repo="http://cran.r-project.org", dep=T, lib=install_libpath)
-install.packages("devtools", repos = "http://cran.us.r-project.org",dependencies=TRUE, lib=install_libpath)
-install.packages("remotes", repos = "http://cran.us.r-project.org",dependencies=TRUE, lib=install_libpath)
+#install.packages("wordcloud2", repo="http://cran.r-project.org", dep=T, lib=install_libpath, type = "binary", type = "binary")
+install.packages("devtools", repos = "http://cran.us.r-project.org",dependencies=TRUE, lib=install_libpath, type = "binary")
+install.packages("remotes", repos = "http://cran.us.r-project.org",dependencies=TRUE, lib=install_libpath, type = "binary")
 
 library(devtools)
 library(remotes)
 
 
-devtools::install_github("lchiffon/wordcloud2", lib=install_libpath)
+devtools::install_github("lchiffon/wordcloud2", lib=install_libpath, type = "binary")
 
-devtools::install_github("ModelOriented/iBreakDown", lib=install_libpath)
-devtools::install_github("ModelOriented/ingredients", lib=install_libpath)
+devtools::install_github("ModelOriented/iBreakDown", lib=install_libpath, type = "binary")
+devtools::install_github("ModelOriented/ingredients", lib=install_libpath, type = "binary")
 
-install.packages("SHAPforxgboost", repos = "http://cran.us.r-project.org",lib=install_libpath)
-devtools::install_github("liuyanguu/SHAPforxgboost", lib=install_libpath)
+install.packages("SHAPforxgboost", repos = "http://cran.us.r-project.org",lib=install_libpath, type = "binary")
+devtools::install_github("liuyanguu/SHAPforxgboost", lib=install_libpath, type = "binary")
 
-install.packages("devtools", repos = "https://cloud.r-project.org/",dependencies=TRUE, lib=install_libpath)
-devtools::install_github("twitter/AnomalyDetection", lib=install_libpath)
+install.packages("devtools", repos = "https://cloud.r-project.org/",dependencies=TRUE, lib=install_libpath, type = "binary")
+devtools::install_github("twitter/AnomalyDetection", lib=install_libpath, type = "binary")
 
-install.packages("rstan", repos = "https://cloud.r-project.org/",dependencies=TRUE, lib=install_libpath)
-install.packages("prophet", repos = "https://cloud.r-project.org/", lib=install_libpath)
+install.packages("rstan", repos = "https://cloud.r-project.org/",dependencies=TRUE, lib=install_libpath, type = "binary")
+install.packages("prophet", repos = "https://cloud.r-project.org/", lib=install_libpath, type = "binary")
 
-devtools::install_github("fisproject/lineNotify", lib=install_libpath)
+devtools::install_github("fisproject/lineNotify", lib=install_libpath, type = "binary")
 
 #########インストール出来なかった場合は別途バイナリーでインストール
-#install.packages("car", repos = "http://cran.us.r-project.org",dependencies=TRUE, lib=install_libpath)
-#install.packages("VIM", repos = "http://cran.us.r-project.org",dependencies=TRUE, lib=install_libpath)
-#install.packages("glmnet", repos = "http://cran.us.r-project.org",dependencies=TRUE, lib=install_libpath)
-#install.packages("forecast", repos = "http://cran.us.r-project.org", lib=install_libpath) 
-#install.packages("ellipse", repos = "http://cran.us.r-project.org", lib=install_libpath) 
+#install.packages("car", repos = "http://cran.us.r-project.org",dependencies=TRUE, lib=install_libpath, type = "binary")
+#install.packages("VIM", repos = "http://cran.us.r-project.org",dependencies=TRUE, lib=install_libpath, type = "binary")
+#install.packages("glmnet", repos = "http://cran.us.r-project.org",dependencies=TRUE, lib=install_libpath, type = "binary")
+#install.packages("forecast", repos = "http://cran.us.r-project.org", lib=install_libpath, type = "binary") 
+#install.packages("ellipse", repos = "http://cran.us.r-project.org", lib=install_libpath, type = "binary") 
 
-#install.packages('dHSIC', repos = "http://cran.us.r-project.org", lib=install_libpath)
+#install.packages('dHSIC', repos = "http://cran.us.r-project.org", lib=install_libpath, type = "binary")
 
-install.packages('processx', repos = "http://cran.us.r-project.org", lib=install_libpath)
+install.packages('processx', repos = "http://cran.us.r-project.org", lib=install_libpath, type = "binary")
 
 ###########################
 
